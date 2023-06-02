cad0a42e8b7b26a6cd795b7f00346dddb8f9a128
Dio Tendean <tendeanclaudio1234@gmail.com>
2023-06-02 21:41:41 +0800

    Initial Commit

---
+++ Covid19App/Covid19AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Covid19AppApp.swift
+//  Covid19App
+//
+//  Created by Claudio Tendean on 02/06/23.
+//
+
+import SwiftUI
+
+@main
+struct Covid19AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
